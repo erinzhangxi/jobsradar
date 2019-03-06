7ff6da85a6636268bcb3b9072770a992e2eab4a7
erinzhangxi <erincheung1101@gmail.com>
2019-03-06 07:54:53 -0500

    Setup db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': jobsradar_db,
+	'USER': 'erin',
+	'PASSOWRD': 'erin_passowrd',
+	'HOST': 'localhost',
+	'PORT': ''
     }
 }
 
